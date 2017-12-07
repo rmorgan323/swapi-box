8cda6ddec924fe4348294c3401e09afb37efdbe9
Rob Morgan <rmorgan323@gmail.com>
2017-12-06 18:08:56 -0700

    gets data and cleans

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,29 +2,106 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from '../Header/Header.js';
 import FilmCrawl from '../FilmCrawl/FilmCrawl.js';
+import CardContainer from '../CardContainer/CardContainer.js';
 
 class App extends Component {
 	constructor() {
 		super();
 
 		this.state = {
-			favorites: ['1', '2'],
-			containerTitle: '',
-			characters: ['char1', 'char2'],
-			worlds: ['world1', 'world2'],
-			vehicles: ['rigs1', 'rigs2']
+			favorites: [],
+			containerTitle: 'select nerdy item',
+			characters: [],
+			worlds: [],
+			vehicles: [],
+			loading: false
 		}
 	}
 
-	getCharacters = () => {
-		console.log('get me some characters')
+	getCharacters = async () => {
+		const fetchChars = await fetch('https://swapi.co/api/people/')
+		const charsObj = await fetchChars.json();
+		console.log(charsObj)
+		const charsArray = charsObj.results.map( async (char) => {
+			const fetchHomeworld = await fetch(char.homeworld);
+			const homeworld = await fetchHomeworld.json();
+			const fetchSpecies = await fetch(char.species[0]);
+			const species = await fetchSpecies.json();
+			return {name: char.name, 
+							homeworld: homeworld.name, 
+							species: species.name, 
+							homePop: homeworld.population}
+		})
+		return Promise.all(charsArray)
+	}
+
+	getWorlds = async () => {
+		const fetchWorlds = await fetch('https://swapi.co/api/planets/')
+		const worldsObj = await fetchWorlds.json();
+		const worldsArray = worldsObj.results.map( async (world) => {
+			const residentArray = await world.residents.map( async (resident) => {
+				const newResident = await fetch(resident);
+				const cleanResident = await newResident.json();
+				const nameToAdd = cleanResident.name;
+				return nameToAdd;
+			});
+			const residentPromises = await Promise.all(residentArray);
+			return {name: world.name,
+							terrain: world.terrain,
+							population: world.population,
+							climate: world.climate,
+							residents: residentPromises}
+		})
+		return Promise.all(worldsArray)
+	}
+
+	getVehicles = async () => {
+		const fetchVehicles = await fetch('https://swapi.co/api/vehicles/')
+		const vehiclesObj = await fetchVehicles.json();
+		const vehiclesArray = vehiclesObj.results.map( async (vehicle) => {
+			return {name: vehicle.name,
+							model: vehicle.model,
+							vClass: vehicle.vehicle_class,
+							passengers: vehicle.passengers}
+		})
+		return Promise.all(vehiclesArray)
+	}
+
+	handleUpdateState = async (updateType) => {
+		let array;
+		if (updateType === 'characters') {
+			this.setState({containerTitle: 'Characters', loading: true, worlds: [], vehicles: []})
+			array = await this.getCharacters();
+		} else if (updateType === 'worlds') {
+			this.setState({containerTitle: 'Worlds', loading: true, characters: [], vehicles: []})
+			array = await this.getWorlds();
+		} else if (updateType === 'vehicles') {
+			this.setState({containerTitle: 'Vehicles', loading: true, characters: [], worlds: []})
+			array = await this.getVehicles();
+		}
+		this.setState({[updateType]: array, loading: false})
+	}
+
+	favoriteCard = (cardObj) => {
+		console.log('hi')
+		console.log(cardObj)
 	}
 
   render() {
     return (
       <div>
-      	<Header favorites={this.state.favorites}
-      					getCharacters={this.getCharacters} />
+		    <Header handleUpdateState={this.handleUpdateState}
+        								favorites={this.state.favorites}
+      									getWorlds={this.getWorlds}
+      								getVehicles={this.getVehicles} />
+
+      	<CardContainer containerTitle={this.state.containerTitle}
+      								 		 characters={this.state.characters}
+		      								 		 worlds={this.state.worlds}
+	      								 		 vehicles={this.state.vehicles}
+	      								 		favorites={this.state.favorites} 
+  								 	 favoriteCardFunc={this.favoriteCard} />
+
     		<FilmCrawl />
       </div>
     );
--- src/helper/helper.js
+++ src/helper/helper.js
@@ -13,35 +13,35 @@ const convertToRomanNumeral = (integer) => {
 }
 
 const pullOpeningCrawl = (rawData) => {
-	// let releaseDate = rawData.release_date.split('-')
-	// const year = releaseDate.shift()
-	// releaseDate.push(year)
+	let releaseDate = rawData.release_date.split('-')
+	const year = releaseDate.shift()
+	releaseDate.push(year)
 
-	// if (releaseDate[0] === '01') {releaseDate[0] = 'January'}
-	// if (releaseDate[0] === '02') {releaseDate[0] = 'February'}
-	// if (releaseDate[0] === '03') {releaseDate[0] = 'March'}
-	// if (releaseDate[0] === '04') {releaseDate[0] = 'April'}
-	// if (releaseDate[0] === '05') {releaseDate[0] = 'May'}
-	// if (releaseDate[0] === '06') {releaseDate[0] = 'June'}
-	// if (releaseDate[0] === '07') {releaseDate[0] = 'July'}
-	// if (releaseDate[0] === '08') {releaseDate[0] = 'August'}
-	// if (releaseDate[0] === '09') {releaseDate[0] = 'September'}
-	// if (releaseDate[0] === '10') {releaseDate[0] = 'October'}
-	// if (releaseDate[0] === '11') {releaseDate[0] = 'November'}
-	// if (releaseDate[0] === '12') {releaseDate[0] = 'December'}
+	if (releaseDate[0] === '01') {releaseDate[0] = 'January'}
+	if (releaseDate[0] === '02') {releaseDate[0] = 'February'}
+	if (releaseDate[0] === '03') {releaseDate[0] = 'March'}
+	if (releaseDate[0] === '04') {releaseDate[0] = 'April'}
+	if (releaseDate[0] === '05') {releaseDate[0] = 'May'}
+	if (releaseDate[0] === '06') {releaseDate[0] = 'June'}
+	if (releaseDate[0] === '07') {releaseDate[0] = 'July'}
+	if (releaseDate[0] === '08') {releaseDate[0] = 'August'}
+	if (releaseDate[0] === '09') {releaseDate[0] = 'September'}
+	if (releaseDate[0] === '10') {releaseDate[0] = 'October'}
+	if (releaseDate[0] === '11') {releaseDate[0] = 'November'}
+	if (releaseDate[0] === '12') {releaseDate[0] = 'December'}
 
-	// const lineBreakRegEx = new RegExp(/\s{3,}/, 'g')
+	const lineBreakRegEx = new RegExp(/\s{3,}/, 'g')
 
-	// const formattedDate = `${releaseDate[0]} ${releaseDate[1]}, ${releaseDate[2]}`
-	// const filmTitle = rawData.title.toUpperCase();
-	// const episodeNo = convertToRomanNumeral(rawData.episode_id);
+	const formattedDate = `${releaseDate[0]} ${releaseDate[1]}, ${releaseDate[2]}`
+	const filmTitle = rawData.title.toUpperCase();
+	const episodeNo = convertToRomanNumeral(rawData.episode_id);
 
-	// const convertLinebreaks = rawData.opening_crawl.replace(lineBreakRegEx, '###');
-	// const openingCrawl = convertLinebreaks.split('###')
+	const convertLinebreaks = rawData.opening_crawl.replace(lineBreakRegEx, '###');
+	const openingCrawl = convertLinebreaks.split('###')
 
-	return { title: "THE FORCE AWAKENS", date: "December 11, 2015", episode: "VII", crawl: ['Luke Skywalker has vanished. In his absence, the sinister FIRST ORDER has risen from the ashes of the Empire and will not rest until Skywalker, the last Jedi, has been destroyed.', 'With the support of the REPUBLIC, General Leia Organa leads a brave RESISTANCE. She is desperate to find her brother Luke and gain his help in restoring peace and justice to the galaxy.', 'Leia has sent her most daring pilot on a secret mission to Jakku, where an old ally has discovered a clue to Lukes whereabouts....'] }
+	// return { title: "THE FORCE AWAKENS", date: "December 11, 2015", episode: "VII", crawl: ['Luke Skywalker has vanished. In his absence, the sinister FIRST ORDER has risen from the ashes of the Empire and will not rest until Skywalker, the last Jedi, has been destroyed.', 'With the support of the REPUBLIC, General Leia Organa leads a brave RESISTANCE. She is desperate to find her brother Luke and gain his help in restoring peace and justice to the galaxy.', 'Leia has sent her most daring pilot on a secret mission to Jakku, where an old ally has discovered a clue to Lukes whereabouts....'] }
 
-	// return { title: filmTitle, date: formattedDate, episode: episodeNo, crawl: openingCrawl };
+	return { title: filmTitle, date: formattedDate, episode: episodeNo, crawl: openingCrawl };
 }
 
 export default pullOpeningCrawl;
